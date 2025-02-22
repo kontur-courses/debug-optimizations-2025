a6f9d5918f3345889d608acd79428fb72c71fe52
kokodio <kirill.zaripow@gmail.com>
2025-02-22 18:34:01 +0500

    Инлайнинг DCT1D, Заранее подсчитать Scale, Плоский Cosine

--- optimizations/JPEG/DCT.cs
+++ optimizations/JPEG/DCT.cs
@@ -6,7 +6,8 @@ namespace JPEG;
 public static class Dct
 {
 	private static readonly double[] Alpha = new double[8];
-	private static readonly double[,] Cosine = new double[8,8];
+	private static readonly double[] Cosine = new double[64];
+	private static readonly double[] Scale = new double[64];
 	private static readonly double Beta = 1d / 8 + 1d / 8;
 	
 	static Dct()
@@ -19,99 +20,77 @@ public static class Dct
 		for(var x = 0; x < 8; x++)
 		for(var u = 0; u < 8; u++)
 		{
-			Cosine[x,u] = Math.Cos(((2d * x + 1d) * u * Math.PI) / (2 * 8));
+			Cosine[x*8+u] = Math.Cos(((2d * x + 1d) * u * Math.PI) / (2 * 8));
+			
+			Scale[x*8+u] = Beta * Alpha[x] * Alpha[u];
 		}
 	}
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	private static void DCT1D(Span<double> input, Span<double> output)
+	private static void DCT1D(Span<double> input, Span<double> output, int offset)
 	{
 		for (var u = 0; u < 8; u++)
 		{
 			var sum = 0.0;
-			for (var x = 0; x < 8; x++)
-			{
-				sum += Cosine[x, u] * input[x];
-			}
-			output[u] = sum * Alpha[u];
+			sum += Cosine[0*8+u] * input[0];
+			sum += Cosine[1*8+u] * input[1];
+			sum += Cosine[2*8+u] * input[2];
+			sum += Cosine[3*8+u] * input[3];
+			sum += Cosine[4*8+u] * input[4];
+			sum += Cosine[5*8+u] * input[5];
+			sum += Cosine[6*8+u] * input[6];
+			sum += Cosine[7*8+u] * input[7];
+			output[u*8+offset] = sum;
 		}
 	}
 	
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static void IDCT1D(Span<double> input, Span<double> output)
+	public static void IDCT1D(Span<double> input, Span<double> output, int offset)
 	{
 		for (var x = 0; x < 8; x++)
 		{
 			var sum = 0.0;
-			for (var u = 0; u < 8; u++)
-			{
-				sum += Cosine[x, u] * input[u];
-			}
-			output[x] = sum;
+			sum += Cosine[x*8+0] * input[0];
+			sum += Cosine[x*8+1] * input[1];
+			sum += Cosine[x*8+2] * input[2];
+			sum += Cosine[x*8+3] * input[3];
+			sum += Cosine[x*8+4] * input[4];
+			sum += Cosine[x*8+5] * input[5];
+			sum += Cosine[x*8+6] * input[6];
+			sum += Cosine[x*8+7] * input[7];
+			output[x*8+offset] = sum;
 		}
 	}
 	
 	public static void DCT2D(Span<double> input, Span<double> output)
 	{
-		Span<double> buffer = stackalloc double[8];
-		Span<double> transformed = stackalloc double[8];
-
 		for (var i = 0; i < 8; i++)
 		{
-			for (var j = 0; j < 8; j++)
-			{
-				buffer[j] = input[i*8+j];
-			}
-			DCT1D(buffer, transformed);
-			for (var j = 0; j < 8; j++)
-			{
-				input[i*8+j] = transformed[j];
-			}
+			DCT1D(input.Slice(i*8,8), output, i);
 		}
-
-		for (var j = 0; j < 8; j++)
+		for (var i = 0; i < 8; i++)
 		{
-			for (var i = 0; i < 8; i++)
-			{
-				buffer[i] = input[i*8+j];
-			}
-			DCT1D(buffer, transformed);
-			for (var i = 0; i < 8; i++)
-			{
-				output[i*8+j] = transformed[i] * Beta;
-			}
+			DCT1D(output.Slice(i*8,8), input, i);
+		}
+		for(var i = 0; i < 64; i++)
+		{
+			output[i] = input[i] * Scale[i];
 		}
 	}
 
-	public static void IDCT2D(Span<double> coeffs, Span<double> output)
+	public static void IDCT2D(Span<double> input, Span<double> output)
 	{
-		Span<double> buffer = stackalloc double[8];
-		Span<double> transformed = stackalloc double[8];
-
-		for (var u = 0; u < 8; u++)
+		for(var i = 0; i < 64; i++)
 		{
-			for (var v = 0; v < 8; v++)
-			{
-				buffer[v] = coeffs[u*8+v] * Alpha[v];
-			}
-			IDCT1D(buffer, transformed);
-			for (var y = 0; y < 8; y++)
-			{
-				coeffs[u*8+y] = transformed[y];
-			}
+			output[i] = input[i] * Scale[i];
 		}
-
-		for (var y = 0; y < 8; y++)
+		for (var i = 0; i < 8; i++)
 		{
-			for (var u = 0; u < 8; u++)
-			{
-				buffer[u] = coeffs[u*8+y] * Alpha[u];
-			}
-			IDCT1D(buffer, transformed);
-			for (var x = 0; x < 8; x++)
-			{
-				output[x*8+y] = transformed[x] * Beta;
-			}
+			IDCT1D(output.Slice(i*8,8), input, i);
+		}
+		for (var i = 0; i < 8; i++)
+		{
+			IDCT1D(input.Slice(i*8,8), output, i);
 		}
 	}
 }
